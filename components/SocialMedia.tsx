b533edb9c8a5ce453597fc98eff35da073e07d51
Shodipo Ayomide <shodipovi@gmail.com>
2022-06-17 12:02:26 +0100

    chore: sort social media

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -8,10 +8,10 @@ const SocialMedia = () => {
   const style = "mx-1 cursor-pointer "
   return (
     <div className='flex flex-row'>      
-      <Facebook className={style} />
-      <Twitter className={style} />
-      <Linkedin className={style} />
-      <Instagram className={style} />
+      <a href="https://www.facebook.com/0xPolygon.Technology/" target="_blank" ><Facebook className={style} /></a>
+      <a href="https://twitter.com/polygonlagos" target="_blank" > <Twitter className={style}/></a>
+      <a href="https://in.linkedin.com/company/0xpolygon" target="_blank" ><Linkedin className={style} /></a>
+      <a href="https://www.instagram.com/0xpolygon/?hl=en" target="_blank" ><Instagram className={style} /></a>
     </div>
   )
 }
