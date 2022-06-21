ed4bbc7548db62a9ff80cb286c65c2445954641b
Shodipo Ayomide <shodipovi@gmail.com>
2022-06-21 22:17:55 +0100

    chore: add images

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
   />
   <meta
     property="og:image"
-    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+    content="/main.png"
   />
   {/* Twitter */}
   <meta property="twitter:card" content="summary_large_image" />
@@ -42,7 +42,7 @@ const Home: NextPage = () => {
   />
   <meta
     property="twitter:image"
-    content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+    content="/main.png"
   />
 </>
 
