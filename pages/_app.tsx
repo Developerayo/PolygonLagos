b25403daf95b1fd77abba4da8755ffcb3a76744c
Shodipo Ayomide <shodipovi@gmail.com>
2022-06-21 23:00:34 +0100

    chore: track page views listening to "routeChangeComplete"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,21 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { useEffect } from "react";
+import { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import * as gtag from "./utils/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />
 }
 
